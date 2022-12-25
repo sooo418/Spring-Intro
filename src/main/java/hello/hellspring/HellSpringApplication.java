6e001645c037f28e5c428c2607d3ec7f392c57ed
sooo418 <ehdwnd02@gmail.com>
2022-12-25 20:42:09 +0900

    Spring 입문 강의 1일차

---
+++ src/main/java/hello/hellspring/HellSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.hellspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellspring/HellSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
