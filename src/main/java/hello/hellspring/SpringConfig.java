3f6b244666fe296145bfdf6c51f26f35e745aced
sooo418 <ehdwnd02@gmail.com>
2022-12-31 02:47:17 +0900

    Spring 입문 강의 6일차

--- src/main/java/hello/hellspring/SpringConfig.java
+++ src/main/java/hello/hellspring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellspring;
 
+import hello.hellspring.repository.JdbcMemberRepository;
 import hello.hellspring.repository.MemberRepository;
 import hello.hellspring.repository.MemoryMemberRepository;
 import hello.hellspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
